bfd8b4b19d9ad39fa93b5ed84b73fbee2a1dd8f4
SpaceEWS-forSchool <47364190+SpaceEWS-forSchool@users.noreply.github.com>
2019-02-05 13:11:59 -0500

    Add files via upload

--- 1-3-9NameGame.py
+++ 1-3-9NameGame.py
@@ -1,14 +1,11 @@
 from __future__ import print_function
 
-'''Alter the code to produce a different output if your name is entered.  As always, start a log file
-test your code.  When it works, upload the code without changing the file name.  Upload the log file to
-the classroom alonf with your code'''
-
 def who_is_this():
-    name=raw_input('Please enter the name of someone in this class:')
+    ''''below is input b/c raw_input doesn't work on mac'''
+    name=input('Please enter the name of someone in this class: ')
     
-    if name=='Carlos':
-        print(name,' favorite phrase is : "Now the fun part is..."')
+    if name=='Eleanor':
+        print('Hiya peeps!!!')
     
     return
     
